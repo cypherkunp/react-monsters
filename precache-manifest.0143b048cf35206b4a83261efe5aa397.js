857800475691b7b55b3e1ab36b7d5b88281e7a8c
Devvrat Shukla <devvratshukla@gmail.com>
2019-08-27 10:09:04 +0530

    Updates

---
+++ precache-manifest.0143b048cf35206b4a83261efe5aa397.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad2c7dc1dcb72254d1ee686bbbc171e0",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "18eeed1614a9c8bc20a1",
+    "url": "/react-monsters/static/css/main.4e592900.chunk.css"
+  },
+  {
+    "revision": "02428decd018e1a0345c",
+    "url": "/react-monsters/static/js/2.16b36f81.chunk.js"
+  },
+  {
+    "revision": "18eeed1614a9c8bc20a1",
+    "url": "/react-monsters/static/js/main.3045801d.chunk.js"
+  },
+  {
+    "revision": "72cbbc18951c23c9c509",
+    "url": "/react-monsters/static/js/runtime~main.62e5349d.js"
+  }
+]);
